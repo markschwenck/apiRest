a816934e8b663d136cd5e6e9e1ad5e13ca232af7
markschwenck <markcell.1@gmail.com>
2025-04-22 21:48:28 -0300

    feat:atualizando a partir da WSL

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,6 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 
-
-
 @Module({
   imports: [UsersModule, PrismaModule],
   controllers: [AppController],
